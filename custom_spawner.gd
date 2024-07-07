84e9e49844dd7ea9d0a97dda15f267fea2fb47b2
Fern <97917922+Fernanman@users.noreply.github.com>
2024-07-07 15:35:02 -0500

    Can use spawnable scenes.

--- custom_spawner.gd
+++ custom_spawner.gd
@@ -7,12 +7,7 @@
 class_name CustomSpawner
 extends Node
 
-# Still need to implement:
-# - When the node is freed on server, freed on all peers. Check!
-# - Spawn all of the players on the a new joiner
-# - Auto spawn scenes.
-
-signal spawned(node: Node)
+signal spawned(node: Node) 	
 signal despawned(node: Node)
 
 @export var spawn_path: NodePath
@@ -24,24 +19,17 @@ var instantiate_function: Callable
 ## The function that determines what data is important and should be extracted
 ## from the scene / node. This is used by spawn_all and can be used by spawn's data parameter.
 var extract_data_function: Callable
-## Dictionary for the authority to keep track of all the scenes / nodes
+## Array for the authority to keep track of all the scenes / nodes
 ## spawned by this node and the peers that have this node spawned as well.
-## Maps to an array, and if the array is empty, then that means it was spawned on all peers.
-var spawned_nodes: Dictionary
-
-func _init():
-	pass
+var spawned_nodes: Array = []
 
 func _ready():
-	if self.has_node(self.spawn_path):
+	if self.has_node(self.spawn_path) and self.is_multiplayer_authority():
 		self.get_node(self.spawn_path).child_exiting_tree.connect(self.call_free)
+		self.get_node(self.spawn_path).child_entered_tree.connect(self.spawn_spawnable_scenes)
 
 	if not self.is_multiplayer_authority():
 		self.ask_spawn()
-
-func _process(_delta):
-	if Input.is_action_just_pressed('ui_accept') and self.is_multiplayer_authority():
-		self.clear_spawnable_scene()
 
 ## Adds the scene / node to the tree at the spawner's current spawn path.
 ## Returns true on succuess and false if the node was not added to tree.
@@ -53,10 +41,8 @@ func _add_scene_to_tree(node: Node) -> bool:
 			spawner_node.add_child(node, true)
 			return true
 
-		print("Node already exists.")
 		return false
 		
-	print("Node at the current spawn path does not exist.")
 	return false
 
 
@@ -69,13 +55,12 @@ func spawn(data: Variant=null, ids: Array=[]):
 	if self.is_multiplayer_authority():
 		var node = self.instantiate_function.call(data)
 		self._add_scene_to_tree(node)
-		self.spawned_nodes[node] = []
+		self.spawned_nodes.append(node)
 		
 		if len(ids) == 0:
 			spawn_clientside.rpc(data)
 		else:
 			for uid in ids:
-				self.spawned_nodes[node].append(uid)
 				spawn_clientside.rpc_id(uid, data)
 
 ## Spawns the all of the peviously spawned nodes on the peer with the given id.
@@ -88,7 +73,7 @@ func spawn_others(id: int):
 			if data:
 				spawn_clientside.rpc_id(id, data)
 			else:
-				print("Data null")
+				print("Could not spawn on peers: Data null")
 
 ## Asks the server to spawn pre existing spawned nodes.
 func ask_spawn():
@@ -127,9 +112,10 @@ func get_spawn_path() -> NodePath:
 
 #region MultiplayerSpawner functions
 
-## Adds a spawnable scene path to the auto spawn list.
+## Adds a spawnable scene path to the auto spawn list if the path is valid.
 func add_spawnable_scene(path: String):
-	self.auto_spawn_list.append(path)
+	if ResourceLoader.exists(path):
+		self.auto_spawn_list.append(path)
 
 ## Despawns all spawnable scenes spawned by this node by freeing them.
 ## May only be called by the authority.
@@ -154,16 +140,23 @@ func get_spawnable_scene_count() -> int:
 ## Calls free on the clients that have this node in their scene tree.
 func call_free(node: Node):
 	if node in self.spawned_nodes:
-		var peers = self.spawned_nodes[node]
-
-		if len(peers) == 0:
-			free_remote.rpc(node.get_path())
-		else:
-			for uid in peers:
-				free_remote.rpc_id(uid, node.get_path())
+		free_remote.rpc(node.get_path())
 
 		self.spawned_nodes.erase(node)
 
+## Spawns spawnable scenes in the spawnable scenes list if one is added to a child of the spawn_path node that was not 
+## spawned by this node.
+func spawn_spawnable_scenes(node: Node):
+	if not node in self.spawned_nodes and self.is_multiplayer_authority():
+		for path in self.auto_spawn_list:
+			var resource = load(path)
+			var node_script = node.get_script()
+
+			if resource is GDScript and node_script == resource \
+			or resource is PackedScene and resource.instantiate().get_script() == node_script:
+				var data = self.extract_data_function.call(node)
+				self.spawned_nodes.append(node)
+				spawn_clientside.rpc(data)
 
 #endregion
 
@@ -173,23 +166,18 @@ func call_free(node: Node):
 ## Spawns the nodes on the client side.
 @rpc("authority", "call_remote", "reliable", 0)
 func spawn_clientside(data: Variant=null):
-	self.spawned.emit()
 	self._add_scene_to_tree(self.instantiate_function.call(data))
+	self.spawned.emit()
 
 ## Frees a node on all remote branches.
 @rpc("authority", "call_remote", "reliable", 0)
 func free_remote(path: NodePath):
-	# Had as queue_free, but can give an Synchronizer error. Most likely because
-	# next time step expects to get the sync from the server.
-	self.despawned.emit()
 	self.get_node(path).free()
+	self.despawned.emit()
 
 ## Grants the request to spawn previous nodes on the requested client.
 @rpc("any_peer", "call_remote", "reliable", 0)
 func grant_spawn():
-	var sender_id = multiplayer.get_remote_sender_id()
-	print("Granting spawn to ", sender_id)
-
-	self.spawn_others(sender_id)
+	self.spawn_others(multiplayer.get_remote_sender_id())
 
 #endregion
